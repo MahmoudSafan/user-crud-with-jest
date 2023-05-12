b2c5c65931170adb65dc5c6fed15aabdecd3a1a8
MahmoudSafan <mahmoudsafan240@gmail.com>
2023-05-12 22:00:07 +0300

    Init Server

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const colors = require("colors");
+const path = require("path");
+const userRouts = require("./routes/users");
+const { errorHandler } = require("./middleware/errorHandler.js");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "views")));
+
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "views", "form.html"));
+});
+
+app.use("/users", userRouts);
+
+app.use(errorHandler);
+
+// catch unhandled rejections
+process.on("unhandledRejection", (error, promise) => {
+	console.log(`Error: ${error.message}`.bgRed.bold);
+	// close server & exit process
+	server.close(() => process.exit(1));
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-const express = require("express");
-const colors = require("colors");
-
+const app = require("./app");
 const dbConnect = require("./config/db");
 const config = require("./config/config");
-
-const app = express();
-
 // Connect to DB
 dbConnect();
 
-app.get("/hello", (req, res) => {
-	res.send("Hello World");
-});
-
 app.listen(config.PORT, () => {
-	console.log(`Server running on port ${config.PORT}`.bgYellow.bold);
+	console.log(`Server running on port ${config.PORT}`.bgGreen.bold);
 });
